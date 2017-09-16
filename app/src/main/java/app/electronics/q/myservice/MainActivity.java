63a368c15159080c46003ee188c0df790d11bde8
Atthana <atthana.p@scale360solutions.com>
2017-09-16 14:41:33 +0700

    Add Fragment

--- app/src/main/java/app/electronics/q/myservice/MainActivity.java
+++ app/src/main/java/app/electronics/q/myservice/MainActivity.java
@@ -3,11 +3,20 @@ package app.electronics.q.myservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.electronics.q.myservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        //Add Fragment
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment())
+                    .commit();
+        }
+
+    }   // main method
+}   //main class
---
+++ app/src/main/java/app/electronics/q/myservice/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package app.electronics.q.myservice.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.electronics.q.myservice.R;
+
+/**
+ * Created by Atthana on 9/16/2017.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+
+    }//Class create fragment view
+}   //Main class
